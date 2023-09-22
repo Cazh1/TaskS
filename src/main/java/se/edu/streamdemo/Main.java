51ca2b2c7dbc46b08c938bd684877f8f262f8224
ZH <cazh1or2@gmail.com>
2023-09-22 17:16:51 +0800

    Update countDeadlines to use stream

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -21,6 +21,7 @@ public class Main {
         printDeadLinesUsingStream(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines (using stream): " + countDeadlinesUsingStream(tasksData));
 
     }
 
@@ -34,7 +35,15 @@ public class Main {
         return count;
     }
 
+    private static int countDeadlinesUsingStream(ArrayList<Task> tasks) {
+        int count = (int) tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .count();
+        return count;
+    }
+
     public static void printAllData(ArrayList<Task> tasksData) {
+        System.out.println("Using iterations");
         for (Task t : tasksData) {
             System.out.println(t);
         }
